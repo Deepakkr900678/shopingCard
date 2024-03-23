55fffac986f7d74a4aeb411ca26dc75c8f6f551c
Deepak Kumar <deepak.groobizz@gmail.com>
2024-03-23 19:05:49 +0530

    improve css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import {FaShoppingCart} from "react-icons/fa";
-
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Navbar() {
-  const {cart} = useSelector((state)=>state);
-  
+  const { cart } = useSelector((state) => state);
+
   return (
     <div>
       <nav className='flex items-center justify-between h-20 max-w-6xl mx-auto'>
         <NavLink to="/">
           <div className='ml-5'>
-            <img src="../logo.jpeg" alt="" className='h-14'/>
+            <img src="../logo.jpeg" alt="" className='h-14' />
           </div>
         </NavLink>
 
@@ -23,13 +22,13 @@ export default function Navbar() {
 
           <NavLink to="/cart">
             <div className='relative'>
-              <FaShoppingCart className='text-2xl'/>
               {
-                cart.length > 0 && 
-                <span className='absolute flex items-start justify-center w-5 h-5 text-xs text-white bg-green-600 rounded right-rounded-full animate-bounce '>
-                 {cart.length} 
+                cart.length > 0 &&
+                <span className='absolute flex items-start justify-center w-4 h-4 text-xs text-white bg-green-600 rounded right-rounded-full animate-bounce'>
+                  {cart.length}
                 </span>
               }
+              <FaShoppingCart className='text-2xl'/>
             </div>
           </NavLink>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Spinner from "../components/Spinner";
 import Product from '../components/Product';
 
@@ -8,39 +8,39 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState([]);
 
-  async function fetchProductData(){
+  async function fetchProductData() {
     setLoading(true);
-    try{
+    try {
       const res = await fetch(API_URL);
       const data = await res.json();
 
       setPosts(data);
-    }catch(error){
+    } catch (error) {
       console.log("Error while fetching data")
       setPosts([]);
     }
     setLoading(false);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchProductData();
   }, [])
 
   return (
     <div>
       {
-        loading ? <Spinner />  :
-        posts.length > 0 ? 
-        (<div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
-          {
-            posts.map( (post) => (
-            <Product key = {post.id} post={post}/>
-          ) )
-          }
-        </div>) :
-        <div className="flex items-center justify-center">
-          <p>No Data Found</p>
-        </div> 
+        loading ? <Spinner /> :
+          posts.length > 0 ?
+            (<div className="grid xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+              {
+                posts.map((post) => (
+                  <Product key={post.id} post={post} />
+                ))
+              }
+            </div>) :
+            <div className="flex items-center justify-center">
+              <p>No Data Found</p>
+            </div>
       }
     </div>
   )
